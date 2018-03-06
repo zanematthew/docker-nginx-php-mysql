8f4943db02f7d2daa11d82ecc87a2294440f6fca
Zane M. Kolnik <zane@zanematthew.com>
2018-03-06 07:25:59 -0500

    Removed backup files, vendor, node modules

--- web/_public/doc/elementlist.js
+++
@@ -1,2 +0,0 @@
-var ApiGen = ApiGen || {};
-ApiGen.elements = [{"file":"namespace-App.Acme.html","label":"App\\Acme"},{"file":"class-App.Acme.Connection.html","label":"App\\Acme\\Connection"},{"file":"class-App.Acme.Foo.html","label":"App\\Acme\\Foo"}];
